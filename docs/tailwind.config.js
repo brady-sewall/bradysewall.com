42268ee1b56d1cfc810c02e8fbc1c53fdb933316
Brady Sewall <brady@apolloneuro.com>
2023-02-16 18:19:24 -0800

    enabling dark mode

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html", "./node_modules/flowbite/**/*.js"],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
